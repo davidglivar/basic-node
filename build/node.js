a2a7433882a0296d74939498a08b43694919332a
David Glivar <davidglivar@gmail.com>
2014-12-22 13:58:27 -0500

    init

---
+++ build/node.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var _extends = function (child, parent) {
+  child.prototype = Object.create(parent.prototype, {
+    constructor: {
+      value: child,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  child.__proto__ = parent;
+};
+
+var EventEmitter = require('events').EventEmitter;
+var Node = (function (EventEmitter) {
+  var Node = function Node() {
+    this.children = [];
+    this.isNode = true;
+    this.parent = null;
+  };
+
+  _extends(Node, EventEmitter);
+
+  Node.prototype.add = function (child, index) {
+    if (this.children.indexOf(child) >= 0 || !this.isNode) return false;
+    if (typeof index === "number") {
+      this.children.splice(index, 0, child);
+    } else {
+      this.children.push(child);
+    }
+    child.parent = this;
+    child.emit("added");
+    return true;
+  };
+
+  Node.prototype.remove = function (child) {
+    var index = this.children.indexOf(child);
+    if (index < 0 || !this.isNode) return false;
+    this.children.splice(index, 1);
+    child.parent = null;
+    child.emit("removed");
+    return true;
+  };
+
+  return Node;
+})(EventEmitter);
+
+exports["default"] = Node;
